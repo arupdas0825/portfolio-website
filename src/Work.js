c9016386819e603496bd7885c11f319d5da29b81
arupdas0825 <dasarup0804@gmail.com>
2026-05-17 12:37:58 +0530

    feat: add hiresight image on work.js

--- src/Work.js
+++ src/Work.js
@@ -32,7 +32,7 @@ const REPO_IMAGES = {
   'quiz-web': '/quiz-web.png',
   'studytra': '/studytra.png',
   'sahasrajit-foundation': '/sahasrajit-foundation.png',
-  'Hiresight-ai': '/Hiresight-ai.png',
+  'Hiresight-ai': '/HireSight-AI.png',
   'LocalCare-Finder-Android': '/LocalCare-Finder-Android.jpeg',
   'NEURAL-RIFT': '/NEURAL-RIFT.png',
   'HyperLane': '/HyperLane.jpeg',
