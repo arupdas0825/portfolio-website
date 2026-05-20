f89214040e0f676b04d3c2db39b4410074f6afff
arupdas0825 <dasarup0804@gmail.com>
2026-05-20 11:17:22 +0530

    feat:fix githubstats.js

--- src/components/LogoItem.jsx
+++
@@ -1,5 +0,0 @@
-// LogoItem is now inlined inside OrbitingLogos.jsx
-// This file kept as stub to avoid import errors
-import { memo } from 'react';
-const LogoItem = () => null;
-export default memo(LogoItem);
