b6622b61dd72141fccb2a2eaaca49d561b253a4b
arupdas0825 <dasarup0804@gmail.com>
2026-07-29 20:54:26 +0530

    Add Canvas 2D polyfill for clean test suite execution

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,6 +5,25 @@ const { TextEncoder, TextDecoder } = require('util');
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
+// Polyfill Canvas 2D context for JSDOM
+HTMLCanvasElement.prototype.getContext = jest.fn(() => ({
+  setTransform: jest.fn(),
+  scale: jest.fn(),
+  font: '',
+  textAlign: '',
+  textBaseline: '',
+  fillStyle: '',
+  measureText: jest.fn(() => ({ width: 100 })),
+  createLinearGradient: jest.fn(() => ({ addColorStop: jest.fn() })),
+  fillText: jest.fn(),
+  getImageData: jest.fn(() => ({ data: new Uint8ClampedArray(400), width: 100, height: 100 })),
+  clearRect: jest.fn(),
+  beginPath: jest.fn(),
+  moveTo: jest.fn(),
+  arc: jest.fn(),
+  fill: jest.fn(),
+}));
+
 // Polyfill ResizeObserver for JSDOM / Three.js Canvas tests
 global.ResizeObserver = class ResizeObserver {
   observe() {}
