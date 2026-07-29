77d5daa13e25f999472649050c529bca9d3208ab
arupdas0825 <dasarup0804@gmail.com>
2026-07-29 20:36:30 +0530

    Fix App test timeout for welcome screen transition

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders App without crashing', async () => {
   render(<App />);
-  const text = await screen.findByText(/arup\.dev/i);
+  const text = await screen.findByText(/Welcome to/i, {}, { timeout: 4000 });
   expect(text).toBeInTheDocument();
 });
